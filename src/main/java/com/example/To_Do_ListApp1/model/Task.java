66e29a5b461e058b2c440c55397b49c75fc3c2de
hemuu <hemuuu183@gmail.com>
2025-12-25 16:01:57 +0530

    new changes

---
+++ src/main/java/com/example/To_Do_ListApp1/model/Task.java
@@ -0,0 +1,22 @@
+package com.example.To_Do_ListApp1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Task {
+    @Id
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+}
--- src/main/java/com/example/To_Do_ListApp1/service/TaskServiceImpl.java
+++ src/main/java/com/example/To_Do_ListApp1/service/TaskServiceImpl.java
@@ -1,28 +1,43 @@
 package com.example.To_Do_ListApp1.service;
 
-import com.example.To_Do_ListApp1.entity.Task;
+import com.example.To_Do_ListApp1.model.Task;
 import com.example.To_Do_ListApp1.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Service
-public class TaskServiceImpl implements TaskService {
+public class TaskServiceImpl {
     @Autowired
     TaskRepository taskRepository;
 
-    // Add task
-    public Task addTask(Task task){
-        return taskRepository.save(task);
+    public void addTask(Task task) {
+        taskRepository.save(task);
     }
 
-//    // Get Tasks
-//    public List<Task> getTask() {
-//        return taskRepository.findAllById();
-//    }
 
-    // Delete Task
-    public void deleteTask(Long Id){
-        taskRepository.deleteById(Id);
+    public String loaded(){
+        List<Task> Tasks = new ArrayList<>(Arrays.asList(
 
+        new Task(1, "Buy groceries", "Milk, Eggs, Bread", "PENDING"),
+
+        new Task(2, "Finish project", "Complete the Spring Boot backend module", "IN_PROGRESS"),
+
+        new Task(3, "Workout", "Evening gym session", "COMPLETED"),
+
+        new Task(4, "Pay bills", "Electricity and Internet bills", "PENDING"),
+
+        new Task(5, "Call mom", "Weekend family catch-up", "PENDING")));
+
+        taskRepository.saveAll(Tasks);
+        return "Loaded";
+
+}
+
+    public List<Task> getAllTask() {
+        return taskRepository.findAll();
     }
 }
